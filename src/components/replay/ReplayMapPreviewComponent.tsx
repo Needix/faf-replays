224da29d0f337e27ae801a633b5d7b1790cf868d
need <need@needix.de>
2025-05-09 23:48:21 +0200

    Refactor: Extract map preview into its own component
    Moved the map preview logic from ReplayPreviewComponent to a new ReplayMapPreviewComponent. This improves modularity and makes the ReplayPreviewComponent cleaner and easier to maintain.

---
+++ src/components/replay/ReplayMapPreviewComponent.tsx
@@ -0,0 +1,35 @@
+import {Card} from "react-bootstrap";
+import StringUtils from "../../utils/StringUtils.ts";
+import {ReplayPreviewComponentProps} from "../types/ReplayPreviewComponentProps.ts";
+import {useState} from "react";
+
+const ReplayMapPreviewComponent = (props: ReplayPreviewComponentProps) => {
+    const data = props.data;
+
+    const completeMapPath = data.mapName ?? "Unknown";
+
+    const [imageError, setImageError] = useState(false);
+    const mapName = completeMapPath.split('/')[2].toLowerCase().replace('.scmap', '');
+    const url = `https://content.faforever.com/maps/previews/large/${mapName}.png`;
+
+    return (<Card className={"preview-card"}>
+        <Card.Title>
+                                <span className={"ms-3 pt-2"}
+                                      style={{fontSize: "20px"}}>Map: {StringUtils.ellipsis(mapName, 25)}</span>
+        </Card.Title>
+        <Card.Body>
+            {!imageError ? (
+                <img
+                    className={"map-preview"}
+                    src={url}
+                    alt={completeMapPath}
+                    onError={() => setImageError(true)}
+                />
+            ) : (
+                <span className="no-image-text">No image</span>
+            )}
+        </Card.Body>
+    </Card>)
+        ;
+}
+export default ReplayMapPreviewComponent;
--- src/components/replay/ReplayPreviewComponent.tsx
+++ src/components/replay/ReplayPreviewComponent.tsx
@@ -1,28 +1,14 @@
-import {Player, ReplayPreviewComponentProps} from "../types/ReplayPreviewComponentProps.ts";
-import {Card, Col, Row, Table} from "react-bootstrap";
-import {useEffect, useState} from "react";
-import LOG from "../../utils/Logger.ts";
-
-import uefLogo from "../../assets/uef.png";
-import aeonLogo from "../../assets/aeon.png";
-import cybranLogo from "../../assets/cybran.png";
-import seraphimLogo from "../../assets/seraphim.png";
-import StringUtils from "../../utils/StringUtils.ts";
+import {ReplayPreviewComponentProps} from "../types/ReplayPreviewComponentProps.ts";
+import {Col, Row} from "react-bootstrap";
 
 import "../css/ReplayPreviewComponent.css";
 import ReplaySummaryGraphs from "./ReplaySummaryGraphs.tsx";
 import ReplayGameDetailsComponent from "./ReplayGameDetailsComponent.tsx";
 import ReplayGameLobbyComponent from "./ReplayGameLobbyComponent.tsx";
+import ReplayMapPreviewComponent from "./ReplayMapPreviewComponent.tsx";
 
 const ReplayPreviewComponent = (props: ReplayPreviewComponentProps) => {
     const data = props.data;
-
-    const completeMapPath = data.mapName ?? "Unknown";
-
-    const [imageError, setImageError] = useState(false);
-    const mapName = completeMapPath.split('/')[2].toLowerCase().replace('.scmap', '');
-    const url = `https://content.faforever.com/maps/previews/large/${mapName}.png`;
-
 
     return (
         <>
@@ -34,24 +20,7 @@ const ReplayPreviewComponent = (props: ReplayPreviewComponentProps) => {
                     <ReplayGameLobbyComponent data={data}/>
                 </Col>
                 <Col sm={"3"}>
-                    <Card className={"preview-card"}>
-                        <Card.Title>
-                                <span className={"ms-3 pt-2"}
-                                      style={{fontSize: "20px"}}>Map: {StringUtils.ellipsis(mapName, 25)}</span>
-                        </Card.Title>
-                        <Card.Body>
-                            {!imageError ? (
-                                <img
-                                    className={"map-preview"}
-                                    src={url}
-                                    alt={completeMapPath}
-                                    onError={() => setImageError(true)}
-                                />
-                            ) : (
-                                <span className="no-image-text">No image</span>
-                            )}
-                        </Card.Body>
-                    </Card>
+                    <ReplayMapPreviewComponent data={data}/>
                 </Col>
             </Row>
             <Row className={"mt-2"}>
