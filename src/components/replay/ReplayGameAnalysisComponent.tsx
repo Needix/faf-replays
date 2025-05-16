8d313bd33d75e9f99d0ea8e8d8187238e4f3898b
need <need@needix.de>
2025-05-16 21:52:19 +0200

    Log props in ReplayGameAnalysisComponent
    Added a console log to output the props passed to the ReplayGameAnalysisComponent. This change aids in debugging and understanding the data being received.

--- src/components/replay/ReplayGameAnalysisComponent.tsx
+++ src/components/replay/ReplayGameAnalysisComponent.tsx
@@ -1,6 +1,7 @@
 import {ReplayPreviewComponentProps} from "../types/ReplayPreviewComponentProps.ts";
 
 const ReplayGameAnalysisComponent = (props: ReplayPreviewComponentProps) => {
+    console.log(props);
     return <div>ReplayGameAnalysisComponent</div>
 }
 export default ReplayGameAnalysisComponent;
