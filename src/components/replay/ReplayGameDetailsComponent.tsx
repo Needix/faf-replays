9a271c8e5ece071f71c8581983bd0244b0663669
need <need@needix.de>
2025-05-18 18:50:03 +0200

    Add replay download functionality to ReplayGameDetailsComponent
    Introduced a download button that allows users to download replay files directly from the game details card. The new `downloadReplay` method handles fetching, blob conversion, and initiating the download process with appropriate error handling. Additionally, adjusted table cell truncation limits for better display of long text.

--- src/components/replay/ReplayGameDetailsComponent.tsx
+++ src/components/replay/ReplayGameDetailsComponent.tsx
@@ -1,10 +1,13 @@
-import {Card, Table} from "react-bootstrap";
+import {Button, Card, Table} from "react-bootstrap";
 import StringUtils from "../../utils/StringUtils.ts";
 import TimeUtils from "../../utils/TimeUtils.ts";
 import {ReplayPreviewComponentProps} from "../types/ReplayPreviewComponentProps.ts";
+import {Api} from "../../api/Api.ts";
 
 const ReplayGameDetailsComponent = (props: ReplayPreviewComponentProps) => {
     const data = props.data;
+
+    const ApiController = new Api().api;
 
     const completeMapPath = data.mapName ?? "Unknown";
 
@@ -19,22 +22,60 @@ const ReplayGameDetailsComponent = (props: ReplayPreviewComponentProps) => {
     const cheatsEnabled = data.cheatsEnabled ?? false;
     const randomSeed = data.randomSeed ?? -1;
 
+    function downloadReplay(replayId: number) {
+        ApiController.getReplayFile(replayId).then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to download replay file: ${response.statusText}`);
+            }
+
+            const fileName = `${replayId}.fafreplay`;
+            const mimeType = 'application/octet-stream';
+
+            // Read the response as a Blob
+            response.blob().then((replayBlob) => {
+                // Create a temporary URL for the downloaded file
+                const fileUrl = URL.createObjectURL(replayBlob);
+
+                // Create an invisible anchor element to trigger the download
+                const a = document.createElement('a');
+                a.href = fileUrl;
+                a.download = fileName;
+                a.type = mimeType;
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+
+                // Revoke the temporary URL after opening
+                URL.revokeObjectURL(fileUrl);
+            }).catch((error) => {
+                console.error('Error viewing replay:', error);
+                alert('An error occurred while trying to download the replay. Please try again.');
+            });
+        });
+    }
+
     return (<Card className={"preview-card"}>
         <Card.Title>
             <span className={"ms-3 pt-2"} style={{fontSize: "20px"}}>Game Stats</span>
         </Card.Title>
         <Card.Body>
             <Table className={"table-sm custom-table"} hover striped borderless>
+                <thead>
+                <tr>
+                    <td style={{width: "15%"}}></td>
+                    <td className="py-0"></td>
+                </tr>
+                </thead>
                 <tbody>
                 <tr>
                     <td className="py-0"><span>Title </span></td>
-                    <td className="py-0"><span title={title}>{StringUtils.ellipsis(title, 60)}</span>
+                    <td className="py-0"><span title={title}>{StringUtils.ellipsis(title, 100)}</span>
                     </td>
                 </tr>
                 <tr>
                     <td className="py-0"><span>Map </span></td>
                     <td className="py-0"><span
-                        title={completeMapPath}>{StringUtils.ellipsis(completeMapPath, 60)}</span></td>
+                        title={completeMapPath}>{StringUtils.ellipsis(completeMapPath, 100)}</span></td>
                 </tr>
                 <tr>
                     <td className="py-0"><span>Duration </span></td>
@@ -88,6 +129,9 @@ const ReplayGameDetailsComponent = (props: ReplayPreviewComponentProps) => {
                 </tr>
                 </tbody>
             </Table>
+            <Button className={"btn-sm float-end me-2"}
+                    onClick={() => downloadReplay(data.id ?? 0)}>
+                Download</Button>
         </Card.Body>
     </Card>);
 }
